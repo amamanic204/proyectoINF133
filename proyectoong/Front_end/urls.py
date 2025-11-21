06e3486657eed9ed25b6cc50e2273ac0625a1f9e
romelengine <romelrobinhoquispecayo@gmail.com>
2025-11-21 10:59:09 -0400

    Se agrega funcionalidades Login

--- proyectoong/Front_end/urls.py
+++ proyectoong/Front_end/urls.py
@@ -5,5 +5,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("", views.index, name = "index"),
     path("login/", auth_views.LoginView.as_view(template_name = "Front_end/login.html"), name = "login"),
-    path("logout/", auth_views.LogoutView.as_view(next_page='login'), name = "logout"),
+    path("logout/", views.cerrar_sesion, name = "logout"),
+    path("registro/", views.registro, name = "registro"),
 ]
--- proyectoong/Front_end/views.py
+++ proyectoong/Front_end/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 # Create your views here.
 
 def index(request):
     return render(request, 'Front_end/index.html')
+
+def cerrar_sesion(request):
+    logout(request)
+    return redirect('index')
+
+def registro (request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            usuario = form.save(commit = False)
+            usuario.is_superuser = False
+            usuario.is_staff = False
+            usuario.save()
+            login(request,usuario)
+            return redirect("principal")
+    else:
+        form = UserCreationForm()
+    return render(request, 'Front_end/registro.html', {'form': form})
